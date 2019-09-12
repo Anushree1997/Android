75d23a1c6259907a38db4e14b55f35686de7a729
Anushree <anushreeawasthi35@gmail.com>
2019-09-13 03:43:08 +0530

    Login Form Using Table Layout

---
+++ LoginFormUsingTableLayout/app/src/main/java/com/example/loginformusingtablelayout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginformusingtablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
