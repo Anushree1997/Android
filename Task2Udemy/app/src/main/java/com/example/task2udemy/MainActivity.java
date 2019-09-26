cdb10538ba29f5f790118c9f51ca001c47d7af81
Anushree <anushreeawasthi35@gmail.com>
2019-09-26 23:44:04 +0530

    Add edit text Send data from edit text to toast

--- Task2Udemy/app/src/main/java/com/example/task2udemy/MainActivity.java
+++ Task2Udemy/app/src/main/java/com/example/task2udemy/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edt_txt;
+
     public void clickFunction(View view){
-        Log.i("Message" , "By On Click Data Is Sending To The Log File");
+
+        edt_txt = (EditText) findViewById(R.id.edt_txt);
+        Toast.makeText(MainActivity.this , edt_txt.getText().toString() + "!" ,Toast.LENGTH_SHORT).show();
     }
 
     @Override
