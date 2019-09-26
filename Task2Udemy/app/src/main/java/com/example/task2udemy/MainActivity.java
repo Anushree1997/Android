dac7bc6a652c357197e4788bd63d05841b3b8a7f
Anushree <anushreeawasthi35@gmail.com>
2019-09-26 19:03:14 +0530

    Added Edit Text Send data to log through edit text

---
+++ Task2Udemy/app/src/main/java/com/example/task2udemy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.task2udemy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        Log.i("Message" , "By On Click Data Is Sending To The Log File");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
