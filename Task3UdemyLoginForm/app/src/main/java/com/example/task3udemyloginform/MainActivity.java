0aa0ea9b4fc9d1e36799749358038055ad92cca0
Anushree <anushreeawasthi35@gmail.com>
2019-09-26 22:18:57 +0530

    Send data from edit text to toast

--- Task3UdemyLoginForm/app/src/main/java/com/example/task3udemyloginform/MainActivity.java
+++ Task3UdemyLoginForm/app/src/main/java/com/example/task3udemyloginform/MainActivity.java
@@ -7,20 +7,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edt_txt1;
-    EditText edt_txt2;
-    Button btn1;
+    EditText edt_txt1 , edt_txt2;
+
+
 
     public void sendData(View view){
 
         edt_txt1 = (EditText) findViewById(R.id.edt_txt1);
         edt_txt2 = (EditText) findViewById(R.id.edt_txt2);
-        Log.i("Uname" , edt_txt1.getText().toString() );
-        Log.i("Password" , edt_txt2.getText().toString());
-    }
+        Toast.makeText(MainActivity.this , edt_txt1.getText().toString() +"  "+ edt_txt2.getText().toString() , Toast.LENGTH_SHORT).show();
+
+     //   Log.i("Uname",edt_txt1.getText().toString());
+      //  Log.i("Pswd", edt_txt2.getText().toString());
+          }
 
 
     @Override
